7ba97868ef1fd67b116c6ccf5f404182356f20a7
Kristof Torfs <kristof.torfs@econoweb.be>
2016-01-05 10:54:21 +0100

    Finished package

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$url = 'https://static.phalconphp.com/www/files/phalcon_x86_vc11_php5.6.0_2.0.9_nts.zip';
+$url64 = 'https://static.phalconphp.com/www/files/phalcon_x64_vc11_php5.6.0_2.0.9_nts.zip';
+
+$packageName = 'phalcon';
+$targetFolder = [io.path]::Combine($(Get-BinRoot), 'php', 'ext')
+$src = [io.path]::Combine($(Get-BinRoot), 'php', 'php.ini-development')
+$ini = [io.path]::Combine($(Get-BinRoot), 'php', 'php.ini')
+if (Test-Path $ini) {
+    $src = $ini
+}
+
+(Get-Content $src) | Foreach-Object {
+$_ -replace ';extension=php_curl.dll', 'extension=php_curl.dll' `
+    -replace ';extension=php_mbstring.dll', 'extension=php_mbstring.dll' `
+    -replace ';extension=php_openssl.dll', "extension=php_openssl.dll`r`nextension=php_phalcon.dll" `
+    -replace '; extension_dir = "ext"', 'extension_dir = "ext"'
+} | Set-Content $ini
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$targetFolder" "$url64"
+Install-ChocolateyPath $targetFolder
+echo ("Phalcon framework installed in " + $targetFolder)
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ini = [io.path]::Combine($(Get-BinRoot), 'php', 'php.ini')
+(Get-Content $ini) | Foreach-Object {
+$_ -replace 'extension=php_phalcon.dll', ''
+} | Set-Content $ini
+$dll = [io.path]::Combine($(Get-BinRoot), 'php', 'ext', 'php_phalcon.dll')
+$lic = [io.path]::Combine($(Get-BinRoot), 'php', 'ext', 'LICENSE.txt')
+Remove-Item -Force $dll
+Remove-Item -Force $lic
